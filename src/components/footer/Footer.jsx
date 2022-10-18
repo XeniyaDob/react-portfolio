763a391807dcf37e24be587d8dfe953ae89dae2f
XeniyaDob <xeniya.dobrogorskaya@gmail.com>
2022-10-18 13:43:28 -0400

    Footer component

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Footer.css";
 const Footer = () => {
-  return <div>Footer</div>;
+  const today = new Date();
+
+  const thisYear = today.getFullYear();
+
+  return (
+    <footer>
+      <a href="#" className="footer_logo">
+        XD &copy;{thisYear}
+      </a>
+    </footer>
+  );
 };
 
 export default Footer;
